3e5611bc4f74bb1d1f7137faf9e7f1f39d99fbb8
MaryamH27 <maryam.rhasnath09@gmail.com>
2024-07-11 14:40:24 -0400

    add coding solution

---
+++ main.swift
@@ -0,0 +1,8 @@
+let appName : String = "TEEMS APP"
+
+var currentVersion : Double = 5.0
+print("My App Name: \(appName)")
+print("Current Version:\(currentVersion)")
+
+currentVersion += 0.5
+print("Current Version: \(currentVersion)")
